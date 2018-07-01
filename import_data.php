7e2f819f2c18af203d28f6c68c3942b88e8bc3e6
Jeremy Ruten <jeremy.ruten@gmail.com>
2018-07-01 11:41:41 -0600

    Import data from XML feed, since the CSV dumps aren't complete now
    This also opens up the possibility of updating the data automatically.

--- import_data.php
+++ import_data.php
@@ -1,6 +1,8 @@
 <?php
 
 $DB_FILE = 'buskatoon.sqlite3';
+$ROUTES_URL = 'http://opendata-saskatoon.cloudapp.net:8080/v1/SaskatoonOpenDataCatalogueBeta/TransitRoutes/';
+$TRIPS_URL = 'http://opendata-saskatoon.cloudapp.net:8080/v1/SaskatoonOpenDataCatalogueBeta/TransitTrips/';
 
 if (file_exists($DB_FILE)) {
   unlink($DB_FILE);
@@ -28,34 +30,45 @@ $db->exec('
 
 $sql = 'INSERT INTO routes (id, short_name, long_name, color) VALUES (:id, :short_name, :long_name, :color);';
 $stmt = $db->prepare($sql);
-$routes_file = fopen("data/routes.txt", "r");
-$routes_header = fgetcsv($routes_file, 1000, ",");
-$ROUTE_ROUTE_ID = array_search('route_id', $routes_header);
-$ROUTE_SHORT_NAME = array_search('route_short_name', $routes_header);
-$ROUTE_LONG_NAME = array_search('route_long_name', $routes_header);
-$ROUTE_COLOR = array_search('route_color', $routes_header);
-while (($row = fgetcsv($routes_file, 1000, ",")) !== FALSE) {
+echo "Retrieving $ROUTES_URL\n";
+$routes = simplexml_load_file($ROUTES_URL);
+foreach ($routes->entry as $route) {
+  $route = $route->content->children('m', true)->properties->children('d', true);
+
+  // Add missing leading 0 that the XML feed seems to chop off
+  $short_name = $route->route_short_name;
+  if (strlen($short_name) == 1) $short_name = "0$short_name";
+
   $stmt->execute([
-    ':id' => $row[$ROUTE_ROUTE_ID],
-    ':short_name' => $row[$ROUTE_SHORT_NAME],
-    ':long_name' => $row[$ROUTE_LONG_NAME],
-    ':color' => $row[$ROUTE_COLOR]
+    ':id' => $route->route_id,
+    ':short_name' => $short_name,
+    ':long_name' => $route->route_long_name,
+    ':color' => $route->route_color
   ]);
 }
-fclose($routes_file);
 
 $sql = 'INSERT INTO trips (id, route_id, headsign) VALUES (:id, :route_id, :headsign);';
 $stmt = $db->prepare($sql);
-$trips_file = fopen("data/trips.txt", "r");
-$trips_header = fgetcsv($trips_file, 1000, ",");
-$TRIP_TRIP_ID = array_search('trip_id', $trips_header);
-$TRIP_ROUTE_ID = array_search('route_id', $trips_header);
-$TRIP_HEADSIGN = array_search('trip_headsign', $trips_header);
-while (($row = fgetcsv($trips_file, 1000, ",")) !== FALSE) {
-  $stmt->execute([
-    ':id' => $row[$TRIP_TRIP_ID],
-    ':route_id' => $row[$TRIP_ROUTE_ID],
-    ':headsign' => $row[$TRIP_HEADSIGN]
-  ]);
-}
-fclose($trips_file);
+
+$trips_url = $TRIPS_URL;
+do {
+  echo "Retrieving $trips_url\n";
+  $trips = simplexml_load_file($trips_url);
+  foreach($trips->getDocNamespaces() as $prefix => $namespace) {
+      if (empty($prefix)) $prefix = "global";
+      $trips->registerXPathNamespace($prefix, $namespace);
+  }
+
+  foreach ($trips->entry as $trip) {
+    $trip = $trip->content->children('m', true)->properties->children('d', true);
+
+    $stmt->execute([
+      ':id' => $trip->trip_id,
+      ':route_id' => $trip->route_id,
+      ':headsign' => $trip->trip_headsign
+    ]);
+  }
+
+  $next_link = $trips->xpath("global:link[@rel='next']");
+  $trips_url = empty($next_link) ? false : (string)$next_link[0]['href'];
+} while ($trips_url);
