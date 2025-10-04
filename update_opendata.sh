e1d0a8d95f817163674d0d652732a1c1089bb676
Paige Ruten <paige.ruten@gmail.com>
2025-10-04 16:10:07 -0600

    Fixes

--- update_opendata.sh
+++ update_opendata.sh
@@ -1,6 +1,4 @@
-rm -rf data
-curl -O http://apps2.saskatoon.ca/app/data/google_transit.zip &&
-  unzip -d data google_transit.zip &&
-  php import_csv.php &&
+cd ~/bin
+php import_data.php &&
   rm -f /srv/www/buskatoon.ca/vehicle_positions.json
 
