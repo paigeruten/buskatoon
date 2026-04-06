4713fd1c0e01b0d179b410eaad892136717aefee
Paige Ruten <paige.ruten@gmail.com>
2026-04-05 19:58:55 -0600

    Visual tweaks, cron fixes

--- update-buskatoon.sh
+++ update-buskatoon.sh
@@ -1,6 +1,17 @@
-(php ~/bin/buskatoon.php /srv/www/buskatoon.ca/vehicle_positions.json) &
-(sleep 10 && php ~/bin/buskatoon.php /srv/www/buskatoon.ca/vehicle_positions.json) &
-(sleep 20 && php ~/bin/buskatoon.php /srv/www/buskatoon.ca/vehicle_positions.json) &
-(sleep 30 && php ~/bin/buskatoon.php /srv/www/buskatoon.ca/vehicle_positions.json) &
-(sleep 40 && php ~/bin/buskatoon.php /srv/www/buskatoon.ca/vehicle_positions.json) &
-(sleep 50 && php ~/bin/buskatoon.php /srv/www/buskatoon.ca/vehicle_positions.json) &
+#!/bin/bash
+
+MAX_RUNTIME=50
+
+while [ $SECONDS -lt $MAX_RUNTIME ]; do
+    START_TIME=$SECONDS
+    php ~/bin/buskatoon.php /srv/www/buskatoon.ca/vehicle_positions.json
+
+    ELAPSED=$((SECONDS - START_TIME))
+    if [ $ELAPSED -lt 10 ]; then
+        SLEEP_TIME=$((10 - ELAPSED))
+        
+        if [ $((SECONDS + SLEEP_TIME)) -lt $MAX_RUNTIME ]; then
+            sleep $SLEEP_TIME
+        fi
+    fi
+done
